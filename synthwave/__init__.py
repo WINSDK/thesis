94da88da21aa6e48de2f46f92b874179640736c8
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2025-04-12 15:03:28 +0200

    Exports.

--- synthwave/__init__.py
+++ synthwave/__init__.py
@@ -1,7 +1,19 @@
 from .helpers import load_model, IS_MACOS, MODEL_DIR
+from .dsl import ExternalError, UOp, Type
+from .dsl import parse, infer, evaluate
 import os
 
 # For some reason unsloth collects statistics when running
 os.environ["UNSLOTH_DISABLE_STATISTICS"] = "1"
 
-__all__ = ['load_model', 'IS_MACOS', 'MODEL_DIR']
+__all__ = [
+    "load_model",
+    "IS_MACOS",
+    "MODEL_DIR",
+    "ExternalError",
+    "UOp",
+    "Type",
+    "parse",
+    "infer",
+    "evaluate"
+]
